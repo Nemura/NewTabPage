4b8e2a18109e4aec90dbb943a28817e30c7df25e
Nemura <lorenzcambre@msn.com>
2018-04-15 16:25:13 +0200

    Rewrote player to work with playlist
    ID of livestream would change every so often which meant you had to manually change the video ID in the code. Tedious...
    Now works with a playlist, and randomizes the starting point in the list. Still kept the option to use single video/stream.

--- src/musicPlayer.js
+++ src/musicPlayer.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 	
 
 	function playMusic(){
-		$("#hiddenVideo").html("<iframe id=\"videoIframe\" width=\"450\" height=\"300\" src=\"https://www.youtube.com/embed/mTSWcjH1ZnU?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>");
+		$("#hiddenVideo").html("<iframe id=\"videoIframe\" width=\"450\" height=\"300\" src=\"http://www.youtube-nocookie.com/embed/" + randomizeMusic() + "\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>");
 		$("#musicButton").empty().append('❚❚');
 		$("#musicButton").off("click").click(pauseMusic);
 	}
@@ -27,4 +27,27 @@ $(document).ready(function() {
 		$("#musicButton").empty().append('►');
 		$("#musicButton").off("click").click(playMusic);;
 	}
+
+	function randomizeMusic(){
+		var outputString = "";
+		var videoID = "XDA7WSMyiro";	//Edit this to use a video ID
+		var playlist = true;	//Switch between video and playlist mode
+		var playlistID = "PLBML8SXyfQ6f0HYiKTs3riLBTaQfO-sGz";	//Edit this to use a playlist and start playing at a random video
+		var playlistLength = "22";	//Enter the amount of video in the playlist manually (Not using the api just to find this out...)
+
+		if (playlist) {
+			var randomIndex = Math.round(Math.random() * (playlistLength - 1));
+			outputString = "?listType=playlist&list=" + 
+							playlistID + 
+							"&amp;index=" + 
+							randomIndex + 
+							"&amp;controls=1&amp;showinfo=0&autoplay=1";
+		} else {
+			outputString = videoID + 
+							"?rel=0&?amp;controls=1&amp;showinfo=0&autoplay=1";
+		}
+
+		return outputString;
+	}
+
 });
